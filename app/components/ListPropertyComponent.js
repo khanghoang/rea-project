0cbf43a439ee420133eb22ef4a8eadf11ba43467
Khang Hoang <hoangtrieukhang@gmail.com>
2016-06-22 23:12:11 +0700

    display the list of property

--- app/components/ListPropertyComponent.js
+++ app/components/ListPropertyComponent.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import PropertyCard from '../components/PropertyCard';
+import _ from 'lodash';
 
 export default class ListPropertyComponent extends Component {
   render() {
     const cards = this.props.properties.map(p => {
       return (
-        <PropertyCard />
+        <PropertyCard
+          key={p.id}
+          agencyPrimaryColor={_.get(p, 'agency.brandingColors.primary', 'white')}
+          mainImage={_.get(p, 'mainImage', '')}
+          agencyLogo={_.get(p, 'agency.logo', '')}
+          price={_.get(p, 'price', 'TBD')}
+          />
       )
     });
 
