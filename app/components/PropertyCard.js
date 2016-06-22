455504bbd3e0172f37c0b0019f15a9572845e765
Khang Hoang <hoangtrieukhang@gmail.com>
2016-06-22 23:08:26 +0700

    improve PropertyCard component and its tests

--- app/components/PropertyCard.js
+++ app/components/PropertyCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 
 export default class PropertyCard extends Component {
   render() {
@@ -6,11 +7,20 @@ export default class PropertyCard extends Component {
       <div
         className='property-card-wrapper'
         onClick={this.props.onClick} >
-        <div className='agency-wrapper'>
+        <div
+          className='agency-wrapper'
+          style={{backgroundColor: this.props.agencyPrimaryColor || 'white'}}
+          >
+            <img
+              className='agency-logo'
+              src={this.props.agencyLogo || ''} />
         </div>
         <div className='property-main-image'>
+          <img
+            src={this.props.mainImage || ''} />
         </div>
         <div className='property-information'>
+          <span>Price: {this.props.price || 'TBD'}</span>
         </div>
       </div>
     )
@@ -18,5 +28,9 @@ export default class PropertyCard extends Component {
 }
 
 PropertyCard.propTypes = {
-  onClick: React.PropTypes.func
+  onClick: React.PropTypes.func,
+  backgroundColor: React.PropTypes.string,
+  agencyLogo: React.PropTypes.string,
+  mainImage: React.PropTypes.string,
+  price: React.PropTypes.string
 }
--- test/components/PropertyCard.js
+++ test/components/PropertyCard.js
@@ -7,38 +7,69 @@ import ReactTestUtils from 'react-addons-test-utils';
 
 describe('<PropertyCard />', () => {
 
-  const card = mount(<PropertyCard />);
-
-  it('should render the card', () => {
-    expect(card.find('.property-card-wrapper').length).to.be.equal(1);
-  });
-
-  it('should render agency', () => {
-    expect(card.find('.agency-wrapper').length).to.be.equal(1);
-  });
-
-  it('should render property main image', () => {
-    expect(card.find('.property-main-image').length).to.be.equal(1);
-  });
-
-  it('should render property information', () => {
-    expect(card.find('.property-information').length).to.be.equal(1);
-  });
-
-  it('should render property information', () => {
-    expect(card.find('.property-information').length).to.be.equal(1);
-  });
-
   it('should call callback function when be tapped', () => {
     const cb = spy();
     const propertyCard = mount(
       <PropertyCard
         onClick={cb}
-      />
+        />
     )
 
     propertyCard.find('.property-card-wrapper').simulate('click');
     expect(cb.calledOnce).to.be.true;
   });
+
+  describe('with mailformed data', () => {
+    const card = mount(<PropertyCard />);
+
+    it('should render agency background with white color as default', () => {
+      expect(card.find('.property-card-wrapper').find('.agency-wrapper')).to.have.style('background-color').equal('white');
+    });
+
+    it('should render agency logo', () => {
+      expect(card.find('.agency-wrapper img')).to.have.attr('src').equal('');
+    });
+
+    it('should render property main image', () => {
+      expect(card.find('.property-main-image img')).to.have.attr('src').equal('');
+    });
+
+    it('should render property information', () => {
+      expect(card.find('.property-information span').text()).to.equal('Price: TBD');
+    });
+  });
+
+  describe('with decent data', () => {
+    const agencyPrimaryColor = 'rgb(51, 51, 51)';
+    const agencyLogo = 'http://i4.au.reastatic.net/agencylogo/BFERIC/12/20150619122858.gif';
+    const mainImage = 'http://i1.au.reastatic.net/640x480/88586227f9176f602d5c19cf06261108dbb29f03e30d1c4ce9fc2b51fb1e4bd6/main.jpg';
+    const price = '$123';
+
+    const card = mount(
+      <PropertyCard
+        agencyPrimaryColor={agencyPrimaryColor}
+        agencyLogo={agencyLogo}
+        mainImage={mainImage}
+        price={price}
+      />
+    );
+
+    it('should render agency background with color', () => {
+      expect(card.find('.property-card-wrapper').find('.agency-wrapper')).to.have.style('background-color').equal(agencyPrimaryColor);
+    });
+
+    it('should render agency logo', () => {
+      expect(card.find('.agency-wrapper img')).to.have.attr('src').equal(agencyLogo);
+    });
+
+    it('should render property main image', () => {
+      expect(card.find('.property-main-image img')).to.have.attr('src').equal(mainImage);
+    });
+
+    it('should render property information', () => {
+      expect(card.find('.property-information span').text()).to.equal(`Price: ${price}`);
+    });
+  })
+
 
 });
