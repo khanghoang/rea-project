5810578ab1dea760c13b415120dcd8cbe5fe0f41
Khang Hoang <hoangtrieukhang@gmail.com>
2016-06-23 22:53:20 +0700

    add list property container

---
+++ app/containers/ListPropertyContainer.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { ListPropertyComponent } from '../components/ListPropertyComponent';
+import nock from 'nock';
+import listProperties from '../../mockData/listProperties';
+
+
+export default class ListPropertyContainer extends Component {
+
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
