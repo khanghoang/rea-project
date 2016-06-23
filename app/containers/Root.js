e5300ca577be4e2e3edd4196f9a4998b89334bb5
Khang Hoang <hoangtrieukhang@gmail.com>
2016-06-23 23:07:27 +0700

    add redux and its minions

--- app/containers/Root.js
+++ app/containers/Root.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from '../reducers';
+
+const store = createStore(reducers);
 
 const root = () => {
   return (
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   )
 }
 
