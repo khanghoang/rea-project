89ae066f825867b204780bfc4710163c5e1f9dae
Khang Hoang <hoangtrieukhang@gmail.com>
2016-06-25 01:02:29 +0700

    add test coverage

--- app/actions/propertyActions.js
+++ app/actions/propertyActions.js
@@ -1,9 +1,9 @@
 import Promise from 'bluebird';
-
-export GET_PROPERTY_LIST = 'GET_PROPERTY_LIST';
-export GET_PROPERTY_LIST_SUCCESS = 'GET_PROPERTY_LIST_SUCCESS';
-export GET_PROPERTY_LIST_FAILURE = 'GET_PROPERTY_LIST_FAILURE';
 import listProperties from '../../mockData/listProperties';
+
+export const GET_PROPERTY_LIST = 'GET_PROPERTY_LIST';
+export const GET_PROPERTY_LIST_SUCCESS = 'GET_PROPERTY_LIST_SUCCESS';
+export const GET_PROPERTY_LIST_FAILURE = 'GET_PROPERTY_LIST_FAILURE';
 
 export const fetchPropertyList = () => {
   return {
--- mockData/listProperties.js
+++ mockData/listProperties.js
@@ -1,4 +1,4 @@
-export default const listProperties = {
+const listProperties = {
   "results": [{
     "price": "$726,500",
     "agency": {
@@ -42,3 +42,5 @@ export default const listProperties = {
     "mainImage": "http://i2.au.reastatic.net/640x480/5e84d96722dda3ea2a084d6935677f64872d1d760562d530c3cabfcb7bcda9c2/main.jpg"
   }]
 }
+
+export default listProperties;
