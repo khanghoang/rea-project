fa7aed3594fa910778a7de4ffd3ddad7341d5815
Khang Hoang <hoangtrieukhang@gmail.com>
2016-07-23 01:00:51 +0700

    fix the test

--- test/containers/App.js
+++ test/containers/App.js
@@ -5,15 +5,18 @@ import Root from '../../app/containers/Root';
 import { expect } from 'chai';
 import React from 'react';
 import sleep from '../utils/sleep';
+import configureStore from '../../app/configureStore';
+
+const store = configureStore({});
 
 describe('<Root />', () => {
   it('should render main app component', () => {
-    const root = mount(<Root />);
+    const root = mount(Root(store));
     expect(root.find('.main-app')).to.exist;
   });
 
   it('changes global variable when click SET DELAY button', async () => {
-    const root = mount(<Root />);
+    const root = mount(Root(store));
 
     await sleep();
     root.find('.enable-delay-button').simulate('click');
@@ -23,7 +26,7 @@ describe('<Root />', () => {
   });
 
   it('changes global variable when click SET FAILURE button', async () => {
-    const root = mount(<Root />);
+    const root = mount(Root(store));
 
     await sleep();
     root.find('.enable-failure-button').simulate('click');
